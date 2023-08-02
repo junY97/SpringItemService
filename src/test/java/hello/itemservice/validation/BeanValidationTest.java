97cc8017a17be29abaf264fb17fa1110a89888d1
junyeong.jo <whwnsdud2024@naver.com>
2023-08-02 14:44:18 +0900

    Bean Validation - 시작

---
+++ src/test/java/hello/itemservice/validation/BeanValidationTest.java
@@ -0,0 +1,42 @@
+package hello.itemservice.validation;
+
+import hello.itemservice.domain.item.Item;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+import java.util.Set;
+
+/**
+ * @author junyeong.jo .
+ * @since 2023-08-02
+ */
+public class BeanValidationTest {
+
+    @BeforeEach
+    public void init() {
+        Locale.setDefault(Locale.KOREA);
+    }
+
+
+    @Test
+    void beanValidation() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        Item item = new Item();
+        item.setItemName(" ");
+        item.setPrice(0);
+        item.setQuantity(10000);
+
+        Set<ConstraintViolation<Item>> violations = validator.validate(item);
+        for (ConstraintViolation<Item> violation : violations) {
+            System.out.println("violation = " + violation);
+            System.out.println("violation.getMessage() = " + violation.getMessage());
+        }
+    }
+}
