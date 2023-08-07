89dd672b4d6559e81c0658885e289a1746409cbd
junyeong.jo <whwnsdud2024@naver.com>
2023-08-07 09:15:00 +0900

    Form 전송 객체 분리 - 개발

---
+++ src/main/java/hello/itemservice/web/validation/form/ItemSaveForm.java
@@ -0,0 +1,27 @@
+package hello.itemservice.web.validation.form;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+/**
+ * @author junyeong.jo .
+ * @since 2023-08-04
+ */
+
+@Data
+public class ItemSaveForm {
+
+    @NotBlank
+    private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
+    private Integer price;
+
+    @NotNull
+    @Max(value = 9999)
+    private Integer quantity;
+}
---
+++ src/main/java/hello/itemservice/web/validation/form/ItemUpdateForm.java
@@ -0,0 +1,27 @@
+package hello.itemservice.web.validation.form;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+/**
+ * @author junyeong.jo .
+ * @since 2023-08-04
+ */
+
+@Data
+public class ItemUpdateForm {
+
+    @NotNull
+    private Long id;
+
+    @NotBlank
+    private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
+    private Integer price;
+
+    private Integer quantity;
+}
