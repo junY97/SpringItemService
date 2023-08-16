66a476fa8df0d1a99a36ecd6a9f53350c28614bc
junyeong.jo <whwnsdud2024@naver.com>
2023-08-16 17:35:53 +0900

    스프링 인터셉터 - 인증 체크

---
+++ src/main/java/hello/itemservice/web/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,32 @@
+package hello.itemservice.web.interceptor;
+
+import hello.itemservice.web.SessionConst;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author junyeong.jo .
+ * @since 2023-08-16
+ */
+
+@Slf4j
+public class LoginCheckInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        HttpSession session = request.getSession(false);
+        String requestURI = request.getRequestURI();
+
+        log.info("인증 체크 인터셉터 실행 {}", requestURI);
+
+        if (session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null) {
+            response.sendRedirect("/login?redirectURL=" + requestURI);
+        }
+
+        return true;
+    }
+}
