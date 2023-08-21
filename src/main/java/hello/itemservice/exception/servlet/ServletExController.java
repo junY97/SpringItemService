5cf0f1c919e01c92bf7f7c0af0286a85ca70591e
junyeong.jo <whwnsdud2024@naver.com>
2023-08-21 16:04:15 +0900

    서블릿 예외 처리 - 오류 페이지 작동 원리

--- src/main/java/hello/itemservice/exception/servlet/ServletExController.java
+++ src/main/java/hello/itemservice/exception/servlet/ServletExController.java
@@ -1,5 +1,6 @@
 package hello.itemservice.exception.servlet;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
