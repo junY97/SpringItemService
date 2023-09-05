5592981d93ed0015bcc3d17d31abda1ba587e31d
junyeong.jo <whwnsdud2024@naver.com>
2023-09-05 17:09:43 +0900

    뷰 템플릿에 컨버터 적용하기

---
+++ src/main/java/hello/itemservice/typeconverter/controller/ConverterController.java
@@ -0,0 +1,49 @@
+package hello.itemservice.typeconverter.controller;
+
+import hello.itemservice.typeconverter.type.IpPort;
+import lombok.Data;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * @author junyeong.jo .
+ * @since 2023-09-05
+ */
+
+@Controller
+public class ConverterController {
+
+    @GetMapping("/converter-view")
+    public String converterView(Model model) {
+        model.addAttribute("number", 1000);
+        model.addAttribute("ipPort", new IpPort("127.0.0.1", 8080));
+        return "typeconverter/converter-view";
+    }
+
+    @GetMapping("/converter/edit")
+    public String converterForm(Model model) {
+        IpPort ipPort = new IpPort("127.0.0.1", 8080);
+        Form form = new Form(ipPort);
+        model.addAttribute("form", form);
+        return "typeconverter/converter-form";
+    }
+
+    @PostMapping("/converter/edit")
+    public String converterEdit(@ModelAttribute Form form, Model model) {
+        IpPort ipPort = form.getIpPort();
+        model.addAttribute("ipPort", ipPort);
+        return "typeconverter/converter-view";
+    }
+
+    @Data
+    static class Form {
+        private IpPort ipPort;
+
+        public Form(IpPort ipPort) {
+            this.ipPort = ipPort;
+        }
+    }
+}
